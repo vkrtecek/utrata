792296baff450a81bcc908b8cdaacb8c281ea794
Štěpán Krteček <krtek@zlin6.cz>
2017-10-06 00:45:14 +0200

    change menu to icons

--- together/scripty/showMenu.php
+++ together/scripty/showMenu.php
@@ -9,35 +9,37 @@ if ( file_exists($promenne) && require($promenne) )
 				<input type="hidden" name="jmeno" value="<?php echo $_REQUEST['j'];?>" />
 				<input type="hidden" name="heslo" value="<?php echo $_REQUEST['h'];?>" />
 				<input type="hidden" name="sekce" value="pridat" />
-				<button type="submit" name="pridat" class="menu" id="addItem"><?=translateByCode($spojeni, 'login', $_REQUEST['j'], 'Menu.AddItem');?></button>
+				<button type="submit" name="pridat" class="menu" id="addItem" title="<?=translateByCode($spojeni, 'login', $_REQUEST['j'], 'Menu.AddItem');?>"></button>
 		</form>
 		<form method="post" action="">
 				<input type="hidden" name="jmeno" value="<?php echo $_REQUEST['j'];?>" />
 				<input type="hidden" name="heslo" value="<?php echo $_REQUEST['h'];?>" />
 				<input type="hidden" name="sekce" value="platba" />
-				<button type="submit" name="platba" class="menu" id="addTransaction"><?=translateByCode($spojeni, 'login', $_REQUEST['j'], 'Menu.AddMoney');?></button>
+				<button type="submit" name="platba" class="menu" id="addTransaction" title="<?=translateByCode($spojeni, 'login', $_REQUEST['j'], 'Menu.AddMoney');?>"></button>
 		</form>
 		<form method="post" action="">
 				<input type="hidden" name="jmeno" value="<?php echo $_REQUEST['j'];?>" />
 				<input type="hidden" name="heslo" value="<?php echo $_REQUEST['h'];?>" />
 				<input type="hidden" name="sekce" value="stare_ucty" />
-				<button type="submit" name="stare_ucty" class="menu" id="oldBills"><?=translateByCode($spojeni, 'login', $_REQUEST['j'], 'Menu.OldItems');?></button>
+				<button type="submit" name="stare_ucty" class="menu" id="oldBills" title="<?=translateByCode($spojeni, 'login', $_REQUEST['j'], 'Menu.OldItems');?>"></button>
 		</form>
 		<form method="post" action="">
 				<input type="hidden" name="jmeno" value="<?php echo $_REQUEST['j'];?>" />
 				<input type="hidden" name="heslo" value="<?php echo $_REQUEST['h'];?>" />
 				<input type="hidden" name="sekce" value="settings" />
-				<button type="submit" name="settings" class="menu" id="settings"><?=translateByCode($spojeni, 'login', $_REQUEST['j'], 'Menu.Settings');?></button>
+				<button type="submit" name="settings" class="menu" id="settings" title="<?=translateByCode($spojeni, 'login', $_REQUEST['j'], 'Menu.Settings');?>"></button>
 		</form>
 		<?php if ( $_REQUEST['sendMonthly'] )
 		{
-			echo '<button style="margin-top:5px;" onClick="motherToggle()">'.translateByCode($spojeni, 'login', $_REQUEST['j'], 'Menu.SendToMother').'</button><br />';
+			echo '<button id="sendToParent" onClick="motherToggle()" title="'.translateByCode($spojeni, 'login', $_REQUEST['j'], 'Menu.SendToMother').'"></button><br />';
 		}?>
-		<button class="menu" onclick="downloadBackUp( '<?php echo $_REQUEST['name']; ?>' )" id="downloadBackUp"><?=translateByCode($spojeni, 'login', $_REQUEST['j'], 'Menu.DownloadBackUp');?></button>
+		<button class="menu" onclick="downloadBackUp( '<?php echo $_REQUEST['name']; ?>' )" id="downloadBackUp" title="<?=translateByCode($spojeni, 'login', $_REQUEST['j'], 'Menu.DownloadBackUp');?>"></button>
 		<form method="post" action="">
-				<button name="logOut" type="submit" class="menu odhlasit" id="logout"><?=translateByCode($spojeni, 'login', $_REQUEST['j'], 'Menu.Logout');?></button>
+				<button name="logOut" type="submit" class="menu odhlasit" id="logout" title="<?=translateByCode($spojeni, 'login', $_REQUEST['j'], 'Menu.Logout');?>"></button>
 		</form>
-		<span id="helpSpan" title="<?=translateByCode($spojeni, 'login', $_REQUEST['j'], 'Menu.ShortcutsTitle');?>" style="cursor:help;"><?=translateByCode($spojeni, 'login', $_REQUEST['j'], 'Menu.Shortcuts');?></span>
+		<div id="overSpan">
+			<span id="helpSpan" title="<?=translateByCode($spojeni, 'login', $_REQUEST['j'], 'Menu.ShortcutsTitle');?>"><?=translateByCode($spojeni, 'login', $_REQUEST['j'], 'Menu.Shortcuts');?></span>
+		</div>
 		<div id="scrollUp"></div>
 		<?php
 	} else echo "<p>Connection with database had failed.</p>";
